093c6ae6be2b3e962b8c7f0f81fc1cf8f69ee34e
mny7240 <mana.078.noa@gmail.com>
2023-09-05 16:26:07 +0000

    Helloの練習

---
+++ app/Models/Diaries.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Diaries extends Model
+{
+    use HasFactory;
+}
