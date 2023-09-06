a12aae135fd451b12d0d0005c5895aaa2c65c8a2
mny7240 <mana.078.noa@gmail.com>
2023-09-06 05:29:19 +0000

    calenderで復習しました

--- app/Http/Controllers/CalenderController.php
+++ app/Http/Controllers/CalenderController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Calender;
 
 class CalenderController extends Controller
 {
-    public function index()
+    public function index(Calender $calendar)
+    {
+        return $calendar->get();
+    }
 }
